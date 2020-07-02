018bf46e523603c0a27b605e8dc017a6e68c563d
Mariana Meireles <mariana@psychonautgirl.space>
2020-07-02 08:26:53 +0200

    Release 1.0.0

--- ipycytoscape/_frontend.py
+++ ipycytoscape/_frontend.py
@@ -9,4 +9,4 @@ Information about the frontend package of the widgets.
 """
 
 module_name = "jupyter-cytoscape"
-module_version = "^0.2.3"
+module_version = "^1.0.0"
--- ipycytoscape/_version.py
+++ ipycytoscape/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Mariana Meireles.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 3)
+version_info = (1, 0, 0)
 __version__ = ".".join(map(str, version_info))
