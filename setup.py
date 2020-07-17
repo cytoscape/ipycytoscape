d1ef87b41f686488af77623c4b4cdfb465cd3a51
ianhi <ianhuntisaak@gmail.com>
2020-07-17 13:27:07 -0400

    update pytest to work with pytest-cov

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ setup_args = dict(
     ],
     extras_require = {
         'test': [
-            'pytest>=3.6',
+            'pytest>4.6',
             'pytest-cov',
             'nbval',
         ],
