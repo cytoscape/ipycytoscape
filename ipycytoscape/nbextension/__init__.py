c7b082708e6dd655cd8dc5b4d49cb0c7ef822cdf
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-11-17 23:23:52 +0100

    update formatting

--- ipycytoscape/nbextension/__init__.py
+++ ipycytoscape/nbextension/__init__.py
@@ -4,10 +4,13 @@
 # Copyright (c) Mariana Meireles
 # Distributed under the terms of the Modified BSD License.
 
+
 def _jupyter_nbextension_paths():
-    return [{
-        'section': 'notebook',
-        'src': 'nbextension/static',
-        'dest': 'jupyter-cytoscape',
-        'require': 'jupyter-cytoscape/extension'
-    }]
+    return [
+        {
+            'section': 'notebook',
+            'src': 'nbextension/static',
+            'dest': 'jupyter-cytoscape',
+            'require': 'jupyter-cytoscape/extension',
+        }
+    ]
