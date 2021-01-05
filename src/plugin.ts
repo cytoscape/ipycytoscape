71a61769fc843a7098b621287e1b84d5a8359afc
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-01-04 22:22:45 -0500

    restore lazy load behavior

--- src/plugin.ts
+++ src/plugin.ts
@@ -10,8 +10,6 @@ import { Widget } from '@phosphor/widgets';
 
 import { IJupyterWidgetRegistry } from '@jupyter-widgets/base';
 
-import * as widgetExports from './widget';
-
 import { MODULE_NAME, MODULE_VERSION } from './version';
 
 const EXTENSION_ID = 'jupyter-cytoscape:plugin';
@@ -38,6 +36,6 @@ function activateWidgetExtension(
   registry.registerWidget({
     name: MODULE_NAME,
     version: MODULE_VERSION,
-    exports: widgetExports,
+    exports: () => import('./widget'),
   });
 }
