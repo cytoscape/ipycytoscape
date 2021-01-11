fc87805da58f878492ee08ddb73600301937f736
Peter Rose <pwrose@ucsd.edu>
2021-01-11 12:27:25 -0800

    added py2neo dependency

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ setup_args = dict(
         "ipywidgets>=7.6.0",
         "spectate>=1.0.0",
         "networkx",
+        "py2neo",
     ],
     extras_require={
         "test": ["pytest>4.6", "pytest-cov", "nbval", "pandas"],
