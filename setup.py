51630620ce90414bbbfd8a3686ca64b3f398d9d7
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-11-26 21:31:12 -0500

    update python version trove classifiers

--- setup.py
+++ setup.py
@@ -90,6 +90,8 @@ setup_args = dict(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Framework :: Jupyter",
     ],
     include_package_data=True,
